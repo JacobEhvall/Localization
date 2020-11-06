f284c443e6b6adeedfefa15f437b4839631658c8
JacobEhvall <55179972+JacobEhvall@users.noreply.github.com>
2020-11-06 10:05:17 +0100

    Commit
    Commiting files, the constrians needs to be fixed.

---
+++ localization/ViewExercies/SelectLanguages.swift
@@ -0,0 +1,15 @@
+//
+//  SelectLanguages.swift
+//  PhotoApp
+//
+//  Created by Jacob  Ehnvall on 2020-10-20.
+//
+
+import UIKit
+ 
+class SelectLanguages: UIViewController {
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+    }
+}
